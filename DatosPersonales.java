37df19710f136d54d162d07f682861ef038631fe
lozpre <taller.sisope.2014@gmail.com>
2016-09-18 14:56:52 -0700

    se agrego metodo de datos

--- DatosPersonales.java
+++ DatosPersonales.java
@@ -3,6 +3,23 @@ import javax.swing.JOptionPane;
 public class DatosPersonales {
 	
 	
+public class DatosPersonales {
+	
+	 String nombre,apellidoPa,apellidoMa;
+	 int edad,dia,mes;
+	 
+	 public void datos(){
+		 
+		 nombre = JOptionPane.showInputDialog("escribe tu nombre");
+		 apellidoPa = JOptionPane.showInputDialog("escribe tu apellido paterno");
+		 apellidoMa = JOptionPane.showInputDialog("escribe tu apellido materno");
+		 
+		 edad = Integer.parseInt(JOptionPane.showInputDialog("escribe tu edad"));
+		 dia = Integer.parseInt(JOptionPane.showInputDialog("escribe el dia en que naciste"));		 
+		 
+		 mes = Integer.parseInt(JOptionPane.showInputDialog("que mes naciste? (escribelo en numero)"));
+
+}
 
 	public static void main(String[] args) {
 			
